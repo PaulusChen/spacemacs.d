a5be3322c76722c4701c6b45222e12ce64fcd930
chenpeng <chenpengsmail@qq.com>
2017-05-15 19:40:47 +0800

    Select utf8 coding system as default

--- layers/chenpeng/config.el
+++ layers/chenpeng/config.el
@@ -2,3 +2,12 @@
 (setq x86-lookup-pdf "~/doc/Intel64-and-IA-32-Architectures-Software-Developers-Manual.pdf")
 
 (my-setup-indent 4)
+
+(set-language-environment 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(setq locale-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-selection-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+
+
