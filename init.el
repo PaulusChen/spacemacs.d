0cb465f3dc58023e48a330072bfacf7f1c0bad26
chenpeng <chenpeng@voole.com>
2018-04-03 12:59:53 +0800

    add irony layer

--- init.el
+++ init.el
@@ -53,15 +53,15 @@ values."
      ;; cscope
      gtags
      chenpeng
+     irony
      asm
      ;; org
-     ;; (shell :variables
-     ;;        shell-default-height 30
-     ;;        shell-default-position 'bottom)
+     (shell :variables
+             shell-default-height 30
+             shell-default-position 'bottom)
      ;; spell-checking8
      ;; syntax-checking
-     ;; version-control
-     chinese
+     version-control
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
